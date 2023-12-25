e85a65b749137c27c10f0e0eca02bbe21657e89d
sheng.wang <justwangsheng@gmail.com>
2023-12-25 10:54:09 +0800

    feat: miyoushe 2.66.1

--- api/common/api.go
+++ api/common/api.go
@@ -9,12 +9,12 @@ import (
 )
 
 const (
-	UserAgent = "Mozilla/5.0 (Linux; Android 13; 22011211C Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 miHoYoBBS/2.62.2"
+	UserAgent = "Mozilla/5.0 (Linux; Android 13; 22011211C Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 miHoYoBBS/" + AppVersion
 
 	RefererApp = "https://app.mihoyo.com"
 	RefererAct = "https://act.mihoyo.com"
 
-	AppVersion    = "2.62.2"
+	AppVersion    = "2.66.1"
 	AppIdMiyoushe = "bll8iq97cem8" // 米游社
 )
 
